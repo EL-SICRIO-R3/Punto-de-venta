d82779ad9311c280d63cce5f0fd2fe78921a3626
EL-SICRIO-R3 <w.leyvapeiro@gmail.com>
2025-02-08 19:15:14 -0700

    feat (primeng); instalacion de prime v19

--- src/app/iterfaces/menu.interface.ts
+++ src/app/iterfaces/menu.interface.ts
@@ -8,7 +8,7 @@ export interface IMenu {
 export const itemsMenu: IMenu[] = [
     { label: 'Pedidos', icon: 'bi bi-clipboard-plus', action: 'pedidos', active: false},
     { label: 'Registrar Platillo', icon: 'bi bi-pencil', action: 'registrarPedido', active: false},
-    { label: 'Registrar Empleado', action: 'registrar-empleado', icon: 'pi pi-users', active: false },
+    { label: 'Registrar Empleado', action: 'registrar-empleado', icon: 'bi bi-person-add', active: false },
     { label: 'Reporte de movimientos', action: 'reporte', icon: 'bi bi-archive', active: false},
     { label: 'Galeria', action: 'galeria', icon: 'bi bi-card-image', active: false}
   ];
