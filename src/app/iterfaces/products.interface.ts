c2aac80cacc472e10126d58877d64152cdc6d250
EL-SICRIO-R3 <w.leyvapeiro@gmail.com>
2025-02-09 11:12:35 -0700

    feat (menuItems): Desarrollo del listado de items del menú

--- src/app/iterfaces/products.interface.ts
+++ src/app/iterfaces/products.interface.ts
@@ -1,8 +1,9 @@
 export interface IProduct {
     id: number;
     nomProduct: string;
-    desciption: string;
-    ptice: number;  
+    description: string;
+    image: string;
+    price: number;  
 }
 
 export interface IListProduct {
@@ -11,3 +12,85 @@ export interface IListProduct {
     table: number;
     total: number;
 }
+
+export interface IOpcFiltroPedido {
+    id: number
+    title: string;
+    icon: string;
+    active: boolean;
+}
+
+export const opcFiltros: IOpcFiltroPedido[] = [
+    {
+        id: 1,
+        title: 'Favoritos',
+        icon: 'pi pi-star',
+        active: false
+    },
+    {
+        id: 2,
+        title: 'Desayunos',
+        icon: 'pi pi-sun',
+        active: false
+    },
+    {
+        id: 3,
+        title: 'Bebidas',
+        icon: 'bi bi-cup-straw',
+        active: false
+    }
+]
+
+
+// Array de productos
+export const products: IProduct[] = [
+    {
+        id: 1,
+        nomProduct: "Orden de birria",
+        description: "Orden de birria enm plato sopero con torillas hechas a mano.",
+        image: "birriaOrden.webp",
+        price: 120
+    },
+    {
+        id: 2,
+        nomProduct: "Quesadilla de birria",
+        description: "Quesadilla de harina o masa rellena de birria.",
+        image: "quesabirria.jpg",
+        price: 60
+    },
+    {
+        id: 3,
+        nomProduct: "Taco Dorado",
+        description: "Taco hecho con torilla de maiz dorado en grasa de birria.",
+        image: "tacoDorado.webp",
+        price: 40
+    },
+    {
+        id: 4,
+        nomProduct: "Coca Cola",
+        description: "Bebida oxigenada de marca cocacola.",
+        image: "coca.jpg",
+        price: 35
+    },
+    {
+        id: 5,
+        nomProduct: "Pizza ded Birria",
+        description: "Pizza normal con el aditivo de birria en la aprte superior",
+        image: "pizzaBirria.jpg",
+        price: 150
+    },
+    {
+        id: 6,
+        nomProduct: "Totta De Birria",
+        description: "Torta de birria con opcon de ahogarla en consomé de birria",
+        image: "tortaBirria.avif",
+        price: 80
+    },
+    {
+        id: 7,
+        nomProduct: "Agua De Sabor",
+        description: "Bebida refrescante de sabor o natural.",
+        image: "bebidasRefrescantes.png",
+        price: 20
+    }
+];
--- src/app/modules/nuevo-pedido/nuevo-pedido.component.ts
+++ src/app/modules/nuevo-pedido/nuevo-pedido.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { IOpcFiltroPedido, IProduct, opcFiltros, products } from '../../iterfaces/products.interface';
 
 @Component({
     selector: 'app-nuevo-pedido',
@@ -11,13 +12,19 @@ export class NuevoPedidoComponent {
 
   ref: DynamicDialogRef | undefined;
   public visible: boolean = false;
-  
+  public ocpFiltros: IOpcFiltroPedido[] = opcFiltros;
+  public itemsProduct: IProduct[] = products;
+
   constructor(public dialogService: DialogService){}
 
 
 
   showDialog() {
     this.visible = true;
+  }
+
+  onSelectItem(item: IOpcFiltroPedido) {
+    this.ocpFiltros.filter(x => x.id === item.id)[0].active = !item.active;
   }
 
 }
